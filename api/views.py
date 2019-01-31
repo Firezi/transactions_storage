dedd2071301d6341fc23558fa9fda4cc10bc59a0
nvalidov <niyazvalidov@gmail.com>
2019-01-31 05:34:21 +0300

    api filters

--- api/views.py
+++ api/views.py
@@ -8,6 +8,19 @@ from api.serializers import TransactionSerializer
 class TransactionView(APIView):
     def get(self, request):
         t = Transaction.objects.all()
+
+        if request.GET.get('fromAddress'):
+            t = t.filter(from_address__iexact=request.GET.get('fromAddress'))
+
+        if request.GET.get('toAddress'):
+            t = t.filter(to_address__iexact=request.GET.get('toAddress'))
+
+        if request.GET.get('fromDate'):
+            t = t.filter(timestamp__gte=request.GET.get('fromDate'))
+
+        if request.GET.get('toDate'):
+            t = t.filter(timestamp__lt=request.GET.get('toDate'))
+
         serializer = TransactionSerializer(t, many=True)
 
         return Response(serializer.data)
